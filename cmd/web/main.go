56e46671a7918d9bddac5f611edf8dd3fc2a7a1c
gabriellend <gdvdsn@gmail.com>
2022-06-09 21:08:46 -0400

    Add static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -17,6 +17,10 @@ func main() {
 	mux.HandleFunc("/cards/swords", showSwords)
 	mux.HandleFunc("/cards/pentacles", showPentacles)
 
+	// Serve files
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Starting server on port 8000")
 	err := http.ListenAndServe(":8000", mux)
 	log.Fatal(err)
